350f77b5d40e159f796829b6f8ad156570b7bc9a
Homunculus-code <68526096+Homunculus-code@users.noreply.github.com>
2022-12-13 12:48:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   axios
-  .get('https://raw.githubusercontent.com/Homunculus-code/databaseforghibli/main/db.json/')
+  .get('https://raw.githubusercontent.com/Homunculus-code/webapp/main/db.json/')
   .then(response => {
     const notes = response.data;
 ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
